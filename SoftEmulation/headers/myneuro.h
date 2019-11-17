985441a272ccad4d0d40c5a1c90ccaec9ad07c49
Sergey Lisitsyn <fox9508@mail.ru>
2019-11-17 19:20:19 +0300

    [TESTS] Add neuro test with template type

---
+++ SoftEmulation/headers/myneuro.h
@@ -0,0 +1,222 @@
+#ifndef MYNEURO_H
+#define MYNEURO_H
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+#define learnRate 0.1
+
+template <class T>
+class myNeuro
+{
+public:
+    myNeuro();
+
+    struct nnLay{
+           int in;
+           int out;
+           T** matrix;
+           T* hidden;
+           T* errors;
+           int getInCount(){return in;}
+           int getOutCount(){return out;}
+           T **getMatrix(){return matrix;}
+           void updMatrix(T *enteredVal)
+           {
+               for(int ou =0; ou < out; ou++)
+               {
+
+                   for(int hid =0; hid < in; hid++)
+                   {
+                       matrix[hid][ou] += (learnRate * errors[ou] * enteredVal[hid]);
+                   }
+                   matrix[in][ou] += (learnRate * errors[ou]);
+               }
+           };
+           void setIO(int inputs, int outputs)
+           {
+               in=inputs;
+               out=outputs;
+               hidden = (T*) malloc((out)*sizeof(T));
+
+               matrix = (T**) malloc((in+1)*sizeof(T*));
+               for(int inp =0; inp < in+1; inp++)
+               {
+                   matrix[inp] = (T*) malloc(out*sizeof(T));
+               }
+               for(int inp =0; inp < in+1; inp++)
+               {
+                   for(int outp =0; outp < out; outp++)
+                   {
+                       matrix[inp][outp] =  ( ((T)rand() / (T)RAND_MAX) - (T)0.5)* (T)pow(out,-0.5);
+                   }
+               }
+           }
+           void makeHidden(T *inputs)
+           {
+               for(int hid =0; hid < out; hid++)
+               {
+                   T tmpS = 0.0;
+                   for(int inp =0; inp < in; inp++)
+                   {
+                       tmpS += inputs[inp] * matrix[inp][hid];
+                   }
+                   tmpS += matrix[in][hid];
+                   hidden[hid] = sigmoida(tmpS);
+               }
+           };
+           T* getHidden()
+           {
+               return hidden;
+           };
+           void calcOutError(T *targets)
+           {
+               errors = (T*) malloc((out)*sizeof(T));
+               for(int ou =0; ou < out; ou++)
+               {
+                   errors[ou] = (targets[ou] - hidden[ou]) * sigmoidasDerivate(hidden[ou]);
+               }
+           };
+           void calcHidError(T *targets,T **outWeights,int inS, int outS)
+           {
+               errors = (T*) malloc((inS)*sizeof(T));
+               for(int hid =0; hid < inS; hid++)
+               {
+                   errors[hid] = 0.0;
+                   for(int ou =0; ou < outS; ou++)
+                   {
+                       errors[hid] += targets[ou] * outWeights[hid][ou];
+                   }
+                   errors[hid] *= sigmoidasDerivate(hidden[hid]);
+               }
+           };
+           T* getErrors()
+           {
+               return errors;
+           };
+           T sigmoida(T val)
+           {
+              return (1.0 / (1.0 + exp((-val).getFloat())));
+           }
+           T sigmoidasDerivate(T val)
+           {
+                return (val * (1.0 - val));
+           };
+    };
+
+    void feedForwarding(bool ok);
+    void backPropagate();
+    void train(T *in, T *targ);
+    void query(T *in);
+    void printArray(T *arr,int s);
+
+private:
+    struct nnLay *list;
+    int inputNeurons;
+    int outputNeurons;
+    int nlCount;
+
+    T *inputs;
+    T *targets;
+};
+
+
+template <class T>
+myNeuro<T>::myNeuro()
+{
+    //--------многослойный
+    inputNeurons = 100;
+    outputNeurons =2;
+    nlCount = 4;
+    list = (nnLay*) malloc((nlCount)*sizeof(nnLay));
+
+    inputs = (T*) malloc((inputNeurons)*sizeof(T));
+    targets = (T*) malloc((outputNeurons)*sizeof(T));
+
+    list[0].setIO(100,20);
+    list[1].setIO(20,6);
+    list[2].setIO(6,3);
+    list[3].setIO(3,2);
+
+    //--------однослойный---------
+//    inputNeurons = 100;
+//    outputNeurons =2;
+//    nlCount = 2;
+//    list = (nnLay*) malloc((nlCount)*sizeof(nnLay));
+
+//    inputs = (T*) malloc((inputNeurons)*sizeof(T));
+//    targets = (T*) malloc((outputNeurons)*sizeof(T));
+
+//    list[0].setIO(100,10);
+//    list[1].setIO(10,2);
+
+}
+
+template <class T>
+void myNeuro<T>::feedForwarding(bool ok)
+{
+    list[0].makeHidden(inputs);
+    for (int i =1; i<nlCount; i++)
+        list[i].makeHidden(list[i-1].getHidden());
+
+    if (!ok)
+    {
+        std::cout <<"Feed Forward: \n";
+        for(int out =0; out < outputNeurons; out++)
+        {
+            std::cout <<list[nlCount-1].hidden[out] <<"\n";
+        }
+        std::cout <<"\n";
+        return;
+    }
+    else
+    {
+       // printArray(list[3].getErrors(),list[3].getOutCount());
+        backPropagate();
+    }
+}
+
+template <class T>
+void myNeuro<T>::backPropagate()
+{
+    //-------------------------------ERRORS-----CALC---------
+    list[nlCount-1].calcOutError(targets);
+    for (int i =nlCount-2; i>=0; i--)
+        list[i].calcHidError(list[i+1].getErrors(),list[i+1].getMatrix(),
+                list[i+1].getInCount(),list[i+1].getOutCount());
+
+    //-------------------------------UPD-----WEIGHT---------
+    for (int i =nlCount-1; i>0; i--)
+        list[i].updMatrix(list[i-1].getHidden());
+    list[0].updMatrix(inputs);
+}
+
+template <class T>
+void myNeuro<T>::train(T *in, T *targ)
+{
+    inputs = in;
+    targets = targ;
+    feedForwarding(true);
+}
+
+template <class T>
+void myNeuro<T>::query(T *in)
+{
+    inputs=in;
+    feedForwarding(false);
+}
+
+template <class T>
+void myNeuro<T>::printArray(T *arr, int s)
+{
+    std::cout <<"__\n";
+    for(int inp =0; inp < s; inp++)
+    {
+        std::cout <<arr[inp] <<"\n";
+    }
+    std::cout <<"\n";
+}
+
+
+
+#endif // MYNEURO_H
--- SoftEmulation/source/neuro.cpp
+++ SoftEmulation/source/neuro.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <iomanip>
 #include "Float32.h"
 #include "Float64.h"
 #include "SoftFloat32.h"
@@ -11,94 +9,57 @@
 #include "half.hpp"
 using half_float::half;
 using namespace half_float::literal;
+#include "myneuro.h"
 
-#define STEPS 20
+typedef SoftPosit32 TYPE;
 
-//#include <cstdio>
-
-int main()
+int main(int argc, char *argv[])
 {
-    /*SoftPosit32 a(-2.0);
-    SoftPosit32 b(1.0);
+    myNeuro<TYPE> *bb = new myNeuro<TYPE>();
 
-    printf("a = %f[%lX;%lX;%lX;%lX;%lX;%lX;%lX;%lX]\n", a.getFloat(), a.val.v0, a.val.v1, a.val.v2, a.val.v3, a.val.v4, a.val.v5, a.val.v6, a.val.v7);
-    printf("b = %f[%lX;%lX;%lX;%lX;%lX;%lX;%lX;%lX]\n", b.getFloat(), b.val.v0, b.val.v1, b.val.v2, b.val.v3, b.val.v4, b.val.v5, b.val.v6, b.val.v7);
-    printf("a-b = %f[%lX;%lX;%lX;%lX;%lX;%lX;%lX;%lX]\n", (a-b).getFloat(), (a-b).val.v0, (a-b).val.v1, (a-b).val.v2, (a-b).val.v3, (a-b).val.v4, (a-b).val.v5, (a-b).val.v6, (a-b).val.v7);
-    */
-    /*SoftPosit8 b(1.0);
-    SoftPosit8 a(2.0);
+     //----------------------------------INPUTS----GENERATOR-------------
+        TYPE *abc = new TYPE[100];
+            for(int i=0; i<100;i++)
+            {
+            abc[i] =(i%98)*0.01+0.01;
+            }
 
-    printf("a = %f [%X]\n", a.getFloat(), a.val.value);
-    printf("b = %f [%X]\n", b.getFloat(), b.val.value);
-    printf("(b/a) = %f [%X]\n", (b/a).getFloat(), (b/a).val.value);*/
+        TYPE *cba = new TYPE[100];
+            for(int i=0; i<100;i++)
+            {
+            cba[i] =((100 - i)%98)*0.01+0.01;
+            }
 
-    /*SoftPosit16 ar(-375.0);
-    SoftPosit16 arg(375.0);
-    SoftPosit16 arg1(108.0);
-    SoftPosit16 arg2(815.0);
-    SoftPosit16 arg3(1500.0);
-    SoftPosit16 x0(4.0);
-    SoftPosit16 x1(4.25);
-    printf("ar = %f [%lX;%lX]\n", ar.getFloat(), ar.val.lvalue, ar.val.rvalue);
-    printf("arg = %f [%lX;%lX]\n", arg.getFloat(), arg.val.lvalue, arg.val.rvalue);
-    printf("-arg = %f [%lX;%lX]\n", (-arg).getFloat(), (-arg).val.lvalue, (-arg).val.rvalue);
-    printf("arg1 = %f [%lX;%lX]\n", arg1.getFloat(), arg1.val.lvalue, arg1.val.rvalue);
-    printf("arg2 = %f [%lX;%lX]\n", arg2.getFloat(), arg2.val.lvalue, arg2.val.rvalue);
-    printf("arg3 = %f [%lX;%lX]\n", arg3.getFloat(), arg3.val.lvalue, arg3.val.rvalue);
-    printf("x0 = %f [%lX;%lX]\n", x0.getFloat(), x0.val.lvalue, x0.val.rvalue);
-    printf("x1 = %f [%lX;%lX]\n", x1.getFloat(), x1.val.lvalue, x1.val.rvalue);
-    printf("-arg3/x0 = %f [%lX;%lX] %f\n", (-arg3/x0).getFloat(), (-arg3/x0).val.lvalue, (-arg3/x0).val.rvalue, 1500.0/4.0);
-    printf("arg2 - arg3/x0 = %f [%lX;%lX] %f\n", (arg2 - arg3/x0).getFloat(), (arg2 - arg3/x0).val.lvalue, (arg2 - arg3/x0).val.rvalue, 815.0 - 1500.0/4.0);
-    printf("(arg2 - arg3/x0)/x1 = %f [%lX;%lX] %f\n", ((arg2 - arg3/x0)/x1).getFloat(), ((arg2 - arg3/x0)/x1).val.lvalue, ((arg2 - arg3/x0)/x1).val.rvalue, (815.0 - 1500.0/4.0)/4.25);
-    printf("arg1 - (arg2 - arg3/x0)/x1 = %f [%lX;%lX] %f\n", (arg1 - (arg2 - arg3/x0)/x1).getFloat(), (arg1 - (arg2 - arg3/x0)/x1).val.lvalue, (arg1 - (arg2 - arg3/x0)/x1).val.rvalue, 108.0 - (815.0 - 1500.0/4.0)/4.25);
-    */
+    //---------------------------------TARGETS----GENERATOR-------------
+        TYPE *tar1 = new TYPE[2];
+        tar1[0] =0.01;
+        tar1[1] =0.99;
+        TYPE *tar2 = new TYPE[2];
+        tar2[0] =0.99;
+        tar2[1] =0.01;
 
-    SoftFloat64 x_f64[STEPS] = {4.0, 4.25};
-    SoftFloat32 x_f32[STEPS] = {4.0, 4.25};
-    half x_f16[STEPS] = {4.0_h, 4.25_h};
-    SoftPosit32 x_pq32[STEPS] = {4.0, 4.25};
-    SoftPosit16 x_pq16[STEPS] = {4.0, 4.25};
-    SoftPosit8 x_pq8[STEPS] = {4.0, 4.25};
-    posit32 x_p32[STEPS] = {4.0, 4.25};
-    posit16 x_p16[STEPS] = {4.0, 4.25};
-    posit8 x_p8[STEPS] = {4.0, 4.25};
-    posit32 x_p32_fma[STEPS] = {4.0, 4.25};
-    posit16 x_p16_fma[STEPS] = {4.0, 4.25};
-    posit8 x_p8_fma[STEPS] = {4.0, 4.25};
+    //--------------------------------NN---------WORKING---------------
+        bb->query(abc);
+        std::cout <<"_________________________________\n";
+        bb->query(cba);
 
-    std::cout << "[##] | S_DOUBLE| S_FLOAT | QPOSIT32| POSIT32'| POSIT32 |  S_HALF | QPOSIT16| POSIT16'| POSIT16| QPOSIT8 |  POSIT8'|  POSIT8 |\n"
-              << "-----------------------------------------------------------------------------------------------------------------------------\n";
-    for (int i = 0; i < STEPS; i++)
-    {
-        if (i >= 2)
+        int i=0;
+        while(i<100000)
         {
-            x_f64[i] = 108.0 - (815.0 - 1500.0/x_f64[i-2])/x_f64[i-1];
-            x_f32[i] = 108.0 - (815.0 - 1500.0/x_f32[i-2])/x_f32[i-1];
-            x_f16[i] = 108.0 - (815.0 - 1500.0/x_f16[i-2])/x_f16[i-1];
-            x_pq32[i] = 108.0 - (815.0 - 1500.0/x_pq32[i-2])/x_pq32[i-1];
-            x_pq16[i] = 108.0 - (815.0 - 1500.0/x_pq16[i-2])/x_pq16[i-1];
-            x_pq8[i] = 108.0 - (815.0 - 1500.0/x_pq8[i-2])/x_pq8[i-1];
-            x_p32[i] = 108.0 - (815.0 - 1500.0/x_p32[i-2])/x_p32[i-1];
-            x_p16[i] = 108.0 - (815.0 - 1500.0/x_p16[i-2])/x_p16[i-1];
-            x_p8[i] = 108.0 - (815.0 - 1500.0/x_p8[i-2])/x_p8[i-1];
-            x_p32_fma[i] = fma(fma((posit32)1500.0, -1.0/x_p32_fma[i-2], (posit32)815.0), -1.0/x_p32_fma[i-1], (posit32)108.0);
-            x_p16_fma[i] = fma(fma((posit16)1500.0, -1.0/x_p16_fma[i-2], (posit16)815.0), -1.0/x_p16_fma[i-1], (posit16)108.0);
-            x_p8_fma[i] = fma(fma((posit8)1500.0, -1.0/x_p8_fma[i-2], (posit8)815.0), -1.0/x_p8_fma[i-1], (posit8)108.0);
+            bb->train(abc,tar1);
+            bb->train(cba,tar2);
+            i++;
+            if (i%10000 == 0)
+                std::cout << i/1000 << "%\n";
         }
-        std::cout << std::setprecision(3)<< std::fixed
-                  << "[" << std::setw(2) << std::setfill('0') << i << "] | "
-                  << std::setw(7) << std::setfill(' ') << x_f64[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_f32[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_pq32[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p32_fma[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p32[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_f16[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_pq16[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p16_fma[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p16[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_pq8[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p8_fma[i] << " | "
-                  << std::setw(7) << std::setfill(' ') << x_p8[i] << " |\n";
-    }
+
+        std::cout <<"___________________RESULT_____________\n";
+        bb->query(abc);
+        std::cout <<"______\n";
+        bb->query(cba);
+
+
+        std::cout <<"_______________THE____END_______________\n";
+
     return 0;
 }
